8cf3822da3927be16f8c07f5cfbe698408331161
Парамонов Арсений Алексеевич <paramonovarseniy@gmail.com>
2026-03-10 23:04:14 +0300

    Реализация задания 3: БД, CRUD и бизнес-операции

---
+++ adboard/src/test/java/ru/rkjrth/adboard/AdboardApplicationTests.java
@@ -0,0 +1,13 @@
+//package ru.rkjrth.adboard;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class AdboardApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+//}
